c9634aa2073fcde314519164efd6d517bef36aa2
Javier Coronas <javier.coronas@movicoders.com>
2022-02-15 23:15:35 +0100

    features/docker: connection between microservices and env vbles

--- public-service/src/controllers/SubscriptionController.ts
+++ public-service/src/controllers/SubscriptionController.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { Response, Request } from 'express';
 import httpStatus from 'http-status';
-import Subscription, { genderType } from '../models/Subscription';
+import Subscription from '../models/Subscription';
 import SubscriptionDetail from '../models/SubscriptionDetail';
 
 export const saveSubscriptionController = async (request: Request, response: Response) => {
     const subscription = request.body as Subscription;
 
     const subscriptionRes = await axios({
-        url: 'http://localhost:40001/subscriptions',
+        url: process.env.URL_SUBSCRIPTIONS as string,
         method: 'POST',
         data: subscription
 
@@ -20,8 +20,10 @@ export const saveSubscriptionController = async (request: Request, response: Res
 
 export const getSubscriptionsController = async (request: Request, response: Response) => {
     console.log("getSubscriptionController")
+    const urrrr = process.env.URL_SUBSCRIPTIONS as string
+    console.log(urrrr)
     const subscriptionRes = await axios({
-        url: 'http://localhost:40001/subscriptions',
+        url: process.env.URL_SUBSCRIPTIONS as string,
         method: 'GET',
         headers: {
             'Authorization': request.headers.authorization || ''
@@ -35,7 +37,7 @@ export const getSubscriptionByIdController = async (request: Request, response: 
     console.log("getSubscriptionByIdController")
     const { id } = request.params
     const subscription = await axios({
-        url: 'http://localhost:40001/subscriptions/' + id,
+        url: process.env.URL_SUBSCRIPTIONS as string + '/' + id,
         method: 'GET',
         headers: {
             'Authorization': request.headers.authorization || ''
@@ -51,7 +53,7 @@ export const cancelSubscriptionByIdController = async (request: Request, respons
     console.log("getSubscriptionByIdController")
     const { id } = request.params
     const subscriptionRes = await axios({
-        url: 'http://localhost:40001/subscriptions/' + id,
+        url: process.env.URL_SUBSCRIPTIONS as string + '/' + id,
         method: 'PUT',
 
     }).then(response => response.data as SubscriptionDetail)
