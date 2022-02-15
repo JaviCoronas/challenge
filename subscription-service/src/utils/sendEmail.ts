937e61b30f6a16cfe3b065b25c40d1e789f6962c
Javier Coronas <javier.coronas@movicoders.com>
2022-02-15 19:28:16 +0100

    master: Hotfix. Send email on subService. Token needed

--- subscription-service/src/utils/sendEmail.ts
+++ subscription-service/src/utils/sendEmail.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { Request } from "express";
+import jwt from 'jwt-simple'
 
 
 
 export function sendEmail(request: Request) {
+    // Create valid token to send through http
+    const jwtsign = process.env.JWT_SECRET as string
+    const token = jwt.encode({ username: "email" }, jwtsign, 'HS256')
     axios({
         url: 'http://localhost:40002/emails',
         method: 'POST',
         headers: {
-            'Authorization': request.headers.authorization || ''
+            'Authorization': 'Bearer ' + token
         },
         data: request.body
     })
