203d22691b5a1971e1fa449e895a5a9c5e905b5a
Javier Coronas <javier.coronas@movicoders.com>
2022-02-15 15:53:56 +0100

    master: First commit with basic structure

---
+++ public-service/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotEnv from 'dotenv';
+
+
+const app = express();
+dotEnv.config();
+app.use(bodyParser.json());
+
+const PORT = process.env.SERVER_PORT;
+
+app.listen(PORT, () => {
+    console.log(`Public server started at port: ${PORT}`);
+});
